0190c7247cb7b871f3e2d313b341fceec53c7a86
ruan <43744645+ruanrrn@users.noreply.github.com>
2025-02-17 01:06:03 +0800

    更新 const.ts
    使用worker环境变量

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
-export const ADDRESS = "YOUR_ADDRESS";
-export const TOKEN = "YOUR_TOKEN";
-export const WORKER_ADDRESS = "YOUR_WORKER_ADDRESS";
+export const ADDRESS = ${env.ALIST_ADDRESS};
+export const TOKEN = ${env.ALIST_TOKEN};
+export const WORKER_ADDRESS = ${env.WORKER_ADDRESS};
